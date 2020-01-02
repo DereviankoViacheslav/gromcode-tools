6999f83ff16d8f664d6dc9992322d67a294958a0
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-02 10:50:44 +0200

    add lesson02 task1 & task2

---
+++ lesson01/task1/dist/scripts/logger/logger.js
@@ -0,0 +1,17 @@
+function createLogger(name) {
+    const logs = [];
+
+    return {
+        log(message) {
+            logs.push(`log - ${name} - ${message}`);
+        },
+        error(errorText) {
+            logs.push(`log - ${name} - ${errorText}`);
+        },
+        getLogs() {
+            return logs;
+        },
+    };
+};
+
+export { createLogger };

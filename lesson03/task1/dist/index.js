10ed094ef8edf053843d24c897459d17b7e52774
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-02 15:29:40 +0200

    add lesson03 task1

--- lesson03/task1/dist/index.js
+++ lesson03/task1/dist/index.js
@@ -4,13 +4,11 @@ require("core-js/modules/es.array.flat");
 
 require("core-js/modules/es.array.unscopables.flat");
 
-require("core-js/modules/es.object.to-string");
-
 require("core-js/modules/es.promise");
 
 require("core-js/modules/es.promise.finally");
 
-var data = [1, 2, [3, 4, [5, 6]]];
-var result = data.flat(2);
+const data = [1, 2, [3, 4, [5, 6]]];
+const result = data.flat(2);
 console.log(result);
 Promise.resolve().finally();
